47962996ed1880d300f9e60d618e7820df8afb02
gouthammallepula <gouthammallepula@gmail.com>
2018-03-06 18:46:44 +0530

    code formatted

--- Repo-Java-Email-App/EMailApp.java
+++ Repo-Java-Email-App/EMailApp.java
@@ -5,27 +5,25 @@ import java.util.Scanner;
 public class EMailApp {
 
 	public static void main(String[] args) {
-		//variables & data
+		// variables & data
 		String fname, lname;
-		
-		//read the data from user
+
+		// read the data from user
 		Scanner inp = new Scanner(System.in);
 		System.out.print("ENter first name : ");
 		fname = inp.nextLine();
 		System.out.print("\nENter last name : ");
 		lname = inp.nextLine();
-		
+
 		// creating object for the Email class and passing data
 		Email em = new Email(fname, lname);
-		
-		//method for setting alternate email
+
+		// method for setting alternate email
 		em.setAltEmail("batman@gmail.com");
 		System.out.println("Your alternate email :  " + em.getAltEmail());
-		
-		//show everything here
+
+		// show everything here
 		System.out.println(em.showinfo());
-		
-		
 
 	}
 
